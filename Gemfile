34a4bce1d31539558dc386d37052ab7048616493
Steven Haddox <steven@haddox.us>
2012-06-11 16:12:27 -0400

    init

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'https://rubygems.org'
+
+gem 'jquery-rails'
+gem 'markdown-rails'
+gem 'pg'
+gem 'rails', '3.2.5'
+
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :development, :test do
+  gem 'capybara'
+  gem 'cucumber-rails'
+  gem 'foreman'
+  gem 'rspec-rails'
+  gem 'yard'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+UscontractingUs::Application.config.secret_token = '176ee73f2457783b6d2a0da55ab5dd3d54b828966eee3e6a51ca77e03d3c711835bc66c276ac1c7acc8fa036fc37a7f17ccb32c13aceb74bf69421405badf1bd'
